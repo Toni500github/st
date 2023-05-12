6ce7ab1442486785e7b77e5034384b3a51c91173
Toni500github <toni500minecraft@gmail.com>
2023-05-12 21:41:47 +0200

    some config stuff

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVu Sans Mono for Powerline:pixelsize=11:antialias=true:autohint=true"; //"Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = /*"DejaVu Sans Mono for Powerline:pixelsize=11:antialias=true:autohint=true";*/ "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -98,7 +98,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.7;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 /*static const char *colorname[] = {
