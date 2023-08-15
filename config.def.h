e4ab748017c50c1ad9afb3c6959f10766bedca17
Toni500github <toni500minecraft@gmail.com>
2023-08-16 00:20:35 +0200

    adding font2 patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = /*"DejaVu Sans Mono for Powerline:pixelsize=11:antialias=true:autohint=true";*/ "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = {
+	"Awesome 5 Free Regular:style=Regular",
+	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
+	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
+};
 static int borderpx = 2;
 
 /*
